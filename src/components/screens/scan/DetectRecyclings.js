e357977066b7c08a4c0f0c233fb88e685ecc4fc0
Weitung Chen <weitung@mit.edu>
2020-09-19 16:25:29 +0800

    integrate react native camera

--- src/components/screens/scan/DetectRecyclings.js
+++ src/components/screens/scan/DetectRecyclings.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import {Button, Text, Appbar, Avatar} from 'react-native-paper';
-import {SafeAreaView, View} from 'react-native';
+// import React from 'react';
+// import {Button, Text, Appbar, Avatar} from 'react-native-paper';
+// import {SafeAreaView, View} from 'react-native';
 
-import {styles} from '../../../styles/form-style';
+// import {styles} from '../../../styles/form-style';
 
-export default function DetectRecyclings() {
-  return (
-    <SafeAreaView style={styles.outer}>
-      <Appbar.Header>
-        <Appbar.Content title="AI" />
-      </Appbar.Header>
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+class DetectRecyclings extends Component{
+  render() {
+    return (
       <View style={styles.container}>
-        <Avatar.Icon size={80} icon="clock" style={styles.avatar} />
-        <Text style={styles.center}>IAN SUCKS! </Text>
-
-        <View style={styles.btn}>
-          <Button>False</Button>
+        <RNCamera
+          style={{ flex: 1, alignItems: 'center' }}
+          ref={ref => {
+            this.camera = ref
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
         </View>
       </View>
-    </SafeAreaView>
-  );
+    )
+  }
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+  
 }
+
+export default DetectRecyclings
