1769cfd5f34936cb31a353f6c7514e176289bd98
Ian Lee <leetinyau15@gmail.com>
2020-09-20 04:31:10 -0400

    WT sucks

--- src/components/screens/plant/ViewPlantScreen.js
+++ src/components/screens/plant/ViewPlantScreen.js
@@ -1,6 +1,42 @@
-import React, {useState} from 'react';
-import {Button, Text} from 'react-native-paper';
+import {
+  Appbar,
+  Avatar,
+  Searchbar,
+  Button,
+  Title,
+  Paragraph,
+} from 'react-native-paper';
 
-export default function ViewPlantScreen() {
-  return <Text>Hello, it's me. 3</Text>;
+import {SafeAreaView, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+
+// Firebase
+import {firebaseApp} from '../../../firebase';
+
+import {styles} from '../../../styles/form-style';
+import {ScrollView} from 'react-native-gesture-handler';
+import {ShowInstructables} from '../art/ShowInstructables';
+
+export default function ViewPlantScreen({navigation}) {
+  // Fetch instructables (async wraps everything into another promise)
+  function getInstructables() {
+    return firebaseApp
+      .database()
+      .ref('/websites/')
+      .once('value')
+      .then(function (snapshot) {
+        return snapshot.val();
+      });
+  }
+
+  return (
+    <SafeAreaView style={styles.outer}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigation.navigate('Home')} />
+        <Appbar.Content titleStyle={styles.center_art} title="VIRTUAL HERE" />
+      </Appbar.Header>
+
+      <View style={styles.container}></View>
+    </SafeAreaView>
+  );
 }
